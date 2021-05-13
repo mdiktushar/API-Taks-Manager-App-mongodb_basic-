f7c6354d4e6f1fedfd816e974f5c2ba64897eeb7
mdiktushar <md.tushar1804@gmail.com>
2021-05-13 23:59:09 +0600

    update mongodb

--- mongodb.js
+++ mongodb.js
@@ -38,20 +38,56 @@ MongoClient.connect(connectionURL,{ useUnifiedTopology: true }, {useNewUrlParser
     //         console.log(user);
     // })
 
-    db.collection('task').findOne({_id: new ObjectID("609c2e19ce70220894692112")}, (error, task) =>{
-        if(error){
-            return console.log("Unable to find");
+    // db.collection('task').findOne({_id: new ObjectID("609c2e19ce70220894692112")}, (error, task) =>{
+    //     if(error){
+    //         return console.log("Unable to find");
+    //     }
+    //     console.log(task);
+    // })
+
+
+
+    // db.collection('task').find({completed: false}).toArray((error, task) =>{
+    //     if(error){
+    //         return console.log("Unable to find");
+    //     }
+    //     console.log(task);
+    // })
+
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID("609c2a07559cab2e207d54c6")
+    // }, {
+    //     $set: {
+    //         name: "Md. Imrul Kayes"
+    //     }
+    // }).then((result) =>{
+    //     console.log(result);
+    // }).catch((error)=>{
+    //    console.log(error); 
+    // })
+
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID("6609c2c86fd268f2ce81d9e74")
+    // }, {
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }).then((result) =>{
+    //     console.log(result);
+    // }).catch((error)=>{
+    //    console.log(error); 
+    // })
+
+    db.collection('task').updateMany({
+       completed: false
+    }, {
+        $set: {
+            completed: true
         }
-        console.log(task);
+    }).then((result) =>{
+        console.log(result);
+    }).catch((error)=>{
+       console.log(error); 
     })
-
-
-
-    db.collection('task').find({completed: false}).toArray((error, task) =>{
-        if(error){
-            return console.log("Unable to find");
-        }
-        console.log(task);
-})
 
 })
