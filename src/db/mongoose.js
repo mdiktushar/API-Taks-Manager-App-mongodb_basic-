47807d00507b94d699e7632913be4c4855dee581
mdiktushar <md.tushar1804@gmail.com>
2021-05-21 00:57:55 +0600

    task write and redat from API

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,17 +6,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-app', {
     useUnifiedTopology: true
 })
 
-const Tasks = mongoose.model('Tasks',{
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false,
-    },
-})
 
 
 // To Run: node src/db/mongoose.js
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Tasks = require('./models/task');
 require('./db/mongoose');
 const User = require('./models/user');
 
@@ -13,10 +14,63 @@ app.post('/users', (req, res) => {
     const user = new User(req.body);
 
     user.save().then(()=>{
-        res.send(user);
+        res.status(201).send(user);
     }).catch((e)=>{
         res.status(400).send(e);
         // res.send(e);
+    })
+})
+
+app.post('/tasks', (req, res) =>{
+    const task = Tasks(req.body);
+    
+    task.save().then(()=>{
+        res.status(201).send(task);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+})
+
+app.get('/users',(req, res)=>{
+    User.find({}).then((users)=>{
+        res.send(users);
+    }).catch((e)=>{
+        res.status(500).send(e);
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+
+app.get('/tasks',(req, res)=>{
+    Tasks.find({}).then((tasks)=>{
+        res.send(tasks);
+    }).catch((e)=>{
+        res.status(500).send(e);
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Tasks.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send(e)
     })
 })
 
@@ -24,3 +78,6 @@ app.listen(port, ()=>{
     console.log('Server is up on post '+port);
 })
 
+//nodemon src/index.js
+//npm run start
+//npm run dev
