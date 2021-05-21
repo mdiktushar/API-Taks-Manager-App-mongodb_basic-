36b5f6e6a84528ce66b2cc0733ca23677e471465
mdiktushar <md.tushar1804@gmail.com>
2021-05-22 00:38:19 +0600

    async

--- playground/promess-chaining.js
+++ playground/promess-chaining.js
@@ -1,16 +1,30 @@
 require('../src/db/mongoose');
+const { count } = require('../src/models/user');
 const User = require('../src/models/user')
 
 // 60a40ced8a65d319241de2d2
 
-User.findByIdAndUpdate('60a54115e7626a2a1cffdd1b',{age: 1}).then((user)=>{
-    console.log(user);
-    return User.countDocuments({age: 1});
-}).then((result)=>{
-    console.log(result);
+// User.findByIdAndUpdate('60a54115e7626a2a1cffdd1b',{age: 1}).then((user)=>{
+//     console.log(user);
+//     return User.countDocuments({age: 1});
+// }).then((result)=>{
+//     console.log(result);
+// }).catch((e)=>{
+//     console.log(e);
+// })
+
+
+const updateAgeAndCount = async (id, age) =>{
+    const user = await User.findByIdAndUpdate(id, {age});
+    const count = await User.countDocuments({age});
+
+    return count;
+}
+
+updateAgeAndCount('60a54115e7626a2a1cffdd1b',2).then((count)=>{
+    console.log(count);
 }).catch((e)=>{
     console.log(e);
 })
-
 
 // node .\playground\promess-chaining.js
--- playground/promess-chaning2.js
+++ playground/promess-chaning2.js
@@ -1,11 +1,25 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('60a541ed3d70fa2124e17bd5').then((task)=>{
-    console.log(task);
-    return Task.countDocuments({completed: false});
-}).then((result)=>{
-    console.log(result);
+// Task.findByIdAndDelete('60a541ed3d70fa2124e17bd5').then((task)=>{
+//     console.log(task);
+//     return Task.countDocuments({completed: false});
+// }).then((result)=>{
+//     console.log(result);
+// }).catch((e)=>{
+//     console.log(e);
+// })
+
+const deleteTaskAndCount = async (id)=>{
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed: false});
+    return count;
+}
+
+deleteTaskAndCount('60a6a35f4fe89603cc45b6dc').then((v)=>{
+    console.log(v);
 }).catch((e)=>{
     console.log(e);
 })
+
+//  node .\playground\promess-chaning2.js
